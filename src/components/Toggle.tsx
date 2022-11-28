3ba5756e4142adccbac5271889fc0facd3020b0b
Jun_53 <58911113+jun-05@users.noreply.github.com>
2022-11-28 20:31:51 +0900

    feat : layout 추가

---
+++ src/components/Toggle.tsx
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import tw from "twin.macro";
+
+const Toggle = ({ toggleFn }: { toggleFn: () => void }) => {
+  const [toggle, setToggle] = useState(false);
+
+  const handleToggle = () => {
+    toggleFn();
+    setToggle((prev) => !prev);
+  };
+
+  return (
+    <div className="flex my-4">
+      <div className="cursor-pointer rounded-ful relative">
+        <ToggleInput
+          type="checkbox"
+          name="toggle1"
+          id="toggle1"
+          toggle={toggle}
+          onClick={handleToggle}
+        />
+        <label
+          htmlFor="toggle1"
+          className="toggle-label dark:bg-gray-700 block w-12 h-4 overflow-hidden rounded-full bg-gray-300 cursor-pointer"
+        />
+      </div>
+    </div>
+  );
+};
+export default Toggle;
+
+// const ToggleInput = styled.input`
+//   ${({ toggle }) =>
+//     toggle ? tw`-translate-x-6 bg-white ` : tw`translate-x-0 bg-blue-400 `};
+//   ${tw`
+//   focus:outline-none
+//   w-6
+//   h-6
+//   rounded-full
+//   absolute
+//   shadow-sm
+//   appearance-none
+//   cursor-pointer
+//   border
+//   top-0
+//   bottom-0
+//   m-auto
+//   ease-in-out
+//   duration-300
+//   transform`}
+// `;
+const ToggleInput = styled.input(({ toggle }: { toggle: boolean }) => [
+  toggle ? tw`-translate-x-6 bg-white ` : tw`translate-x-0 bg-blue-400 `,
+  tw`
+  focus:outline-none
+  w-6
+  h-6
+  rounded-full
+  absolute
+  shadow-sm
+  appearance-none
+  cursor-pointer
+  border
+  top-0
+  bottom-0
+  m-auto
+  ease-in-out
+  duration-300
+  transform`,
+]);
